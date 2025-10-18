ae45268aff0ef0fc109e1df79d9826abeadebe1e
Vibol Sen <144869855+VibolSen@users.noreply.github.com>
2025-10-18 17:16:15 +0700

    feat: Add Study Office Dashboard page

---
+++ app/study-office/dashboard/page.jsx
@@ -0,0 +1,11 @@
+
+const StudyOfficeDashboardPage = () => {
+  return (
+    <div>
+      <h1>Study Office Dashboard</h1>
+      <p>Welcome to the Study Office Dashboard!</p>
+    </div>
+  );
+};
+
+export default StudyOfficeDashboardPage;
