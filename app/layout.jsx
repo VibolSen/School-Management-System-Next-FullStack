e4298d5535cbbba39d4a41593f570413db3a9826
Vibol Sen <144869855+VibolSen@users.noreply.github.com>
2025-10-21 22:36:48 +0700

    icon tap

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { UserProvider } from '@/context/UserContext';
+import { UserProvider } from "@/context/UserContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "School Management System",
   description: "School Management System",
-  // Add this 'icons' object back in, pointing to your PNG file
   icons: {
-    icon: "/logo/favicon2.ico",
+    icon: "/favicon.ico", // ✅ points to /public/favicon.ico
   },
 };
 
@@ -17,9 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
-        <UserProvider>
-          {children}
-        </UserProvider>
+        <UserProvider>{children}</UserProvider>
       </body>
     </html>
   );
